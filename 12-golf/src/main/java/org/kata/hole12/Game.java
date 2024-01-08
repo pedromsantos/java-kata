ae00a8682ebc5dfac575e291c59f4b9a754fcc63
Pedro Santos <pedro.santos.personal@gmail.com>
2024-01-08 12:25:07 +0100

    feat: add hole 12

---
+++ 12-golf/src/main/java/org/kata/hole12/Game.java
@@ -0,0 +1,46 @@
+package org.kata.hole12;
+
+public class Game {
+    private final Board board = new Board();
+    private char lastPlayer = ' ';
+
+    public char Winner() {
+        return board.anyRowHasSamePlayerOnAllColumns();
+    }
+
+    public void play(Tile tile) throws Exception {
+
+        validateFirstMove(tile.getPlayer());
+        validatePlayer(tile.getPlayer());
+        validatePosition(tile);
+
+        updatePlayer(tile.getPlayer());
+        updateBoard(tile);
+    }
+
+    private void validateFirstMove(char player) throws Exception {
+        if (lastPlayer == ' ' && player == 'O') {
+            throw new Exception("Invalid first player");
+        }
+    }
+
+    private void validatePlayer(char player) throws Exception {
+        if (player == lastPlayer) {
+            throw new Exception("Invalid next player");
+        }
+    }
+
+    private void validatePosition(Tile tile) throws Exception {
+        if (board.TileAt(tile).isNotEmpty()) {
+            throw new Exception("Invalid position");
+        }
+    }
+
+    private void updatePlayer(char player) {
+        lastPlayer = player;
+    }
+
+    private void updateBoard(Tile tile) {
+        board.AddTileAt(tile);
+    }
+}
---
+++ 12-golf/src/main/java/org/kata/hole12/Tile.java
@@ -0,0 +1,35 @@
+package org.kata.hole12;
+
+public class Tile {
+    private final Position position;
+    private char player;
+
+    public Tile(int x, int y, char player) {
+        this.position = new Position(x, y);
+        this.player = player;
+    }
+
+    public Tile(int x, int y) {
+        this(x, y, ' ');
+    }
+
+    public boolean isNotEmpty() {
+        return this.getPlayer() != ' ';
+    }
+
+    public boolean hasSamePlayerAs(Tile other) {
+        return this.getPlayer() == other.getPlayer();
+    }
+
+    public boolean hasSamePositionAs(Tile other) {
+        return this.position.equals(other.position);
+    }
+
+    public char getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(char player) {
+        this.player = player;
+    }
+}
