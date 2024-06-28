07a367b879d8c68dde29808046b13e35c8703720
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-06-28 17:12:45 +0200

    fix: typos

--- 9-mars-rover/src/test/java/org/kata/MarsRoverAcceptanceTests.java
+++ 9-mars-rover/src/test/java/org/kata/MarsRoverAcceptanceTests.java
@@ -5,23 +5,23 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class MarsRoverAcceptanceTests {
-    @Test
-    void turn_360_degrees_clokwise_while_moving_around_the_plateau() {
-				String commands = "5 5\n1 2 N\nLMLMLMLMM";
-				MarsRover rover = new MarsRover();
+	@Test
+	void turn_360_degrees_clockwise_while_moving_around_the_plateau() {
+		String commands = "5 5\n1 2 N\nLMLMLMLMM";
+		MarsRover rover = new MarsRover();
 
-				String finalPosition = rover.execute(commands);
+		String finalPosition = rover.execute(commands);
 
-        assertThat(finalPosition).isEqualTo(commands);
-    }
+		assertThat(finalPosition).isEqualTo(commands);
+	}
 
-		@Test
-    void turn_360_degrees_counter_clokwise_while_moving_around_the_plateau() {
-				String commands = "5 5\n1 2 N\nMMRMMRMRRM";
-				MarsRover rover = new MarsRover();
+	@Test
+	void turn_360_degrees_counter_clockwise_while_moving_around_the_plateau() {
+		String commands = "5 5\n1 2 N\nMMRMMRMRRM";
+		MarsRover rover = new MarsRover();
 
-				String finalPosition = rover.execute(commands);
+		String finalPosition = rover.execute(commands);
 
-        assertThat(finalPosition).isEqualTo(commands);
-    }
+		assertThat(finalPosition).isEqualTo(commands);
+	}
 }
