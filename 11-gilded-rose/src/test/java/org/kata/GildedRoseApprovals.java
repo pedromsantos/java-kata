e3048986f0cb84042c464b8dd3a3476446138540
Pedro Santos <pedromsantos@users.noreply.github.com>
2024-08-15 20:27:00 +0200

    feat: update open api

--- 11-gilded-rose/src/test/java/org/kata/GildedRoseApprovals.java
+++ 11-gilded-rose/src/test/java/org/kata/GildedRoseApprovals.java
@@ -7,17 +7,18 @@ import org.lambda.functions.Function3;
 class GildedRoseApprovals {
 
 	Function3<String, Integer, Integer, String> doUpdateQuality = (name, sellIn, quality) -> {
-		GildedRose app = new GildedRose(new Item[]{new Item(name, sellIn, quality)});
+		GildedRose app = new GildedRose(new Item[] { new Item(name, sellIn, quality) });
 		app.updateQuality();
 		return app.items[0].toString();
 	};
 
 	@Test
 	void coverAll() {
-		// String[] itemNames = {"Aged Brie", "foo", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"};
-		// Integer[] sellInValues = {-1, 0, 1, 6, 11};
-		// Integer[] qualityValues = {-1, 0, 1, 49, 50};
+		String[] itemNames = { "Aged Brie", "foo", "Backstage passes to a TAFKAL80ETC concert",
+				"Sulfuras, Hand of Ragnaros" };
+		Integer[] sellInValues = { -1, 0, 1, 6, 11 };
+		Integer[] qualityValues = { -1, 0, 1, 49, 50 };
 
-		// CombinationApprovals.verifyAllCombinations(doUpdateQuality, itemNames, sellInValues, qualityValues);
+		CombinationApprovals.verifyAllCombinations(doUpdateQuality, itemNames, sellInValues, qualityValues);
 	}
 }
