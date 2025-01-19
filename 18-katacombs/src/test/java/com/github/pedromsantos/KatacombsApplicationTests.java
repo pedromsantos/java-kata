468ee53bcee131c5a1059cb7e1b6c5485da55b4a
Andrzej Prochyra <andrzej@prochyra.io>
2025-01-19 17:01:06 +0000

    test: add explanatory comment to empty starter test

--- 18-katacombs/src/test/java/com/github/pedromsantos/KatacombsApplicationTests.java
+++ 18-katacombs/src/test/java/com/github/pedromsantos/KatacombsApplicationTests.java
@@ -8,6 +8,7 @@ class KatacombsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// Verifies that the application context loads without errors, ensuring the basic configuration is correct.
 	}
 
 }
