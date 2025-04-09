091038c0dba1fe3777ca46ae58394dee89ae6dbe
Jaafar Fora <jaafarfora@gmail.com>
2025-04-09 14:27:31 +0100

    refactor implementation for non 3 and non 5 numbers

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,12 +2,6 @@ package org.kata;
 
 public class FizzBuzz {
     public String evaluate(int number) {
-        if (number == 1) {
-            return "1";
-        }
-        if (number == 11) {
-            return "11";
-        }
-        return "2";
+        return number + "";
     }
 }
