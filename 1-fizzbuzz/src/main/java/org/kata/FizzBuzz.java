6628a0bed7d0e4c69eee9055c0e2a7bd03745fdc
Jaafar Fora <jaafarfora@gmail.com>
2025-04-09 14:46:54 +0100

    refactor implementation for multiples of 3s using modulo

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.kata;
 
 public class FizzBuzz {
     public String evaluate(int number) {
-        if (number == 3 || number == 6 || number == 12) {
+        if (number % 3 == 0) {
             return "Fizz";
         }
         return number + "";
