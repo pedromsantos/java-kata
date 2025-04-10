dc0a20ce54b8fc1a55abead172c05cf5c90f693d
Jaafar Fora <jaafarfora@gmail.com>
2025-04-10 07:15:07 +0100

    pass test for number 5 to return Buzz

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,6 +2,9 @@ package org.kata;
 
 public class FizzBuzz {
     public String evaluate(int number) {
+        if (number == 5) {
+            return "Buzz";
+        }
         if (number % 3 == 0) {
             return "Fizz";
         }
