ced3cae67ff019b728d58df7d3a9a2cd471797de
Jaafar Fora <jaafarfora@gmail.com>
2025-04-10 07:19:00 +0100

    refactor implementation for multiples of 5 using modulo

--- 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
+++ 1-fizzbuzz/src/main/java/org/kata/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.kata;
 
 public class FizzBuzz {
     public String evaluate(int number) {
-        if (number == 5 || number == 10 || number == 55) {
+        if (number % 5 == 0) {
             return "Buzz";
         }
         if (number % 3 == 0) {
